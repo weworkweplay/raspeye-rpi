66cf93896330cec0ae5e7d823b0b2d60ad9b19d7
Pieter Beulque <pieterbeulque@gmail.com>
2014-03-11 12:24:48 +0100

    Change camera to continuous streaming of pictures.

--- camera.js
+++ camera.js
@@ -7,7 +7,6 @@ var sys = require('sys'),
       username: 'demo',
       password: 'demo'
     },
-    burstMode = true,
     takePicture,
     uploadPicture,
     syncPictures;
@@ -19,7 +18,7 @@ var sys = require('sys'),
  * @param string fileName
  * @return void
  */
-uploadPicture = function (fileName) {
+uploadPicture = function (fileName, callback) {
   if (api.length === 0) {
     return false;
   }
@@ -33,6 +32,8 @@ uploadPicture = function (fileName) {
       if (response.statusCode === 200) {
         fs.unlink(fileName, function (err) {});
       }
+
+      takePicture();
     }).auth(auth.username, auth.password, true));
   });
 };
@@ -40,18 +41,17 @@ uploadPicture = function (fileName) {
 /**
  * Execute default raspistill command, saving picture in /images/
  *
- * @param function callback - Gets filename as an argument
  * @return void
  */
 takePicture = function (callback) {
   var now = new Date(),
       fileName = '/home/pi/images/' + now.getTime() + '.jpg';
 
-  exec('raspistill -o ' + fileName + ' -w 1920 -h 1080 -q 15', function (err, stdin, stdout) {});
-
-  setTimeout(function () {
-    callback(fileName);
-  }, 10000);
+  exec('raspistill -o ' + fileName + ' -w 1920 -h 1080 -q 15', function (err, stdin, stdout) {
+    if (!err) {
+      uploadPicture(fileName);
+    }
+  });
 };
 
 /**
@@ -81,29 +81,4 @@ syncPictures = function () {
 
 // Ready for take-off!
 syncPictures();
-
-(function timelapse () {
-  var now = new Date(),
-      timeout;
-
-  // Default timeout = 3 minutes
-  timeout = 180000;
-
-  // If night (between 7PM and 7AM), timeout = 10 minutes
-  if (now.getHours() < 7 || now.getHours() > 19) {
-    timeout = 600000;
-  }
-
-  // If burst mode is on, timeout = 1 seconds
-  if (burstMode) {
-    timeout = 1000;
-  }
-
-  takePicture(uploadPicture);
-  setTimeout(timelapse, timeout);
-})();
-
-// Let burst mode run for 3 minutes to allow camera positioning
-setTimeout(function () {
-  burstMode = false;
-}, 180000);
+takePicture();
